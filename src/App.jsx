7eb73a4df3a60cadd4777b2948c87cd6b5a43d84
karanjiShiva1 <shivashankarkaranji@gmail.com>
2024-05-21 09:15:46 +0530

    solu

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import React from 'react'
+
+function App(){
+  
+
+  return (
+    <>
+    <h1 className="bg-red-500 text">Hello world</h1>
+    </>
+  )
+}
+
+export default App
